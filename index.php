9d49994dc6a9dd98f4322c52eb145082ead6e31b
HENRY <morgan.henry13@gmail.com>
2016-09-12 14:05:46 +0200

    Optimisation structure html

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Morgan
+ * Date: 12/09/2016
+ * Time: 13:48
+ */
+require_once('structure/head.html');
+
+?>
+
+<body>
+
+<div id="warp">
+
+    <?php require_once('structure/menu.html'); ?>
+
+    <?php require_once('structure/banner.html'); ?>
+    <?php #require_once('structure/banner2.html'); ?>
+
+    <?php require_once('structure/about_us.html'); ?>
+    <?php require_once('structure/portfolio.html'); ?>
+    <?php require_once('structure/tarif.html'); ?>
+    <?php require_once('structure/sponsors.html'); ?>
+    <?php require_once('structure/news.html'); ?>
+    <?php require_once('structure/contact.html'); ?>
+
+
+
+</div>
+
+
+<?php require_once('structure/footer.html'); ?>
+
+</body>
+</html>
+
